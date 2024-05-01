59db675a8742e7e5fe1496afbd98af472349168c
Kalin Ovtcharov <kovtchar@amd.com>
2024-05-01 16:57:20 -0700

    Added .env and loading env variable. Fixed message type.

--- Neo/bot.py
+++ Neo/bot.py
@@ -1,6 +1,9 @@
 import re
 import time
 import sys
+import openai
+import os
+from dotenv import load_dotenv
 from io import StringIO
 from llama_index.core.tools import FunctionTool
 from llama_index.llms.openai import OpenAI
@@ -40,7 +43,7 @@ def custom_query(agent, query):
     messages = []
     response = remove_color_formatting(captured_output.getvalue())
     tps = len(response.split()) / elapsed_time
-    valid_message_types = ["thought", "action Input", "observation", "answer"]
+    valid_message_types = ["thought", "action input", "observation", "answer"]
     for line in response.split("\n"):
         message_type = line.split(":")[0].lower()
         if message_type in valid_message_types:
@@ -59,6 +62,8 @@ def custom_query(agent, query):
 multiply_tool = FunctionTool.from_defaults(fn=multiply)
 
 # initialize llm
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
 llm = OpenAI(model="gpt-3.5-turbo-0613")
 
 # initialize ReAct agent
