e21a4cfc1733dcb3273676e9f98cba26cdf3ea81
Kalin Ovtcharov <kalin@extropolis.ai>
2024-05-21 02:31:44 -0700

    Working version of Neo, github query engine.

--- agents/Neo/bot.py
+++ agents/Neo/bot.py
@@ -23,6 +23,16 @@ from botbuilder.schema import ChannelAccount, Activity
 from llm.npu_llm import LocalLLM
 
 
+def extract_github_owner_repo(message):
+    github_link_pattern = r'https?://(?:www\.)?github\.com/([a-zA-Z0-9_-]+)/([a-zA-Z0-9_-]+)'
+    match = re.search(github_link_pattern, message)
+    if match:
+        owner = match.group(1)
+        repo = match.group(2)
+        return owner, repo
+    else:
+        return None, None
+
 # define sample Tool
 def multiply(a: int, b: int) -> int:
     """Multiply two integers and returns the result integer"""
@@ -109,16 +119,21 @@ def create_repo_engine(owner: str, repo: str) -> QueryEngineTool:
     repo_index.storage_context.persist(persist_dir="./storage/repo")
 
     global repo_engine
-    repo_engine = repo_index.as_query_engine(similarity_top_k=3)
-
-    global repo_tool
-    repo_tool = QueryEngineTool(
-        query_engine=repo_engine,
-        metadata=ToolMetadata(
-            name=f"{owner}/{repo}",
-            description=(f"Provides information about {owner}/{repo} code repository. " "Use a detailed plain text question as input to the tool."),
-        ),
+    repo_engine = repo_index.as_query_engine(
+        verbose=True,
+        similarity_top_k=1,
+        response_mode="compact",
+        streaming=True,
     )
+
+    # global repo_tool
+    # repo_tool = QueryEngineTool(
+    #     query_engine=repo_engine,
+    #     metadata=ToolMetadata(
+    #         name=f"{owner}/{repo}",
+    #         description=(f"Provides information about {owner}/{repo} code repository. " "Use a detailed plain text question as input to the tool."),
+    #     ),
+    # )
 
     return f"Successfully created {owner}/{repo} repo index and tools!"
 
@@ -159,61 +174,80 @@ Settings.embed_model = "local:BAAI/bge-base-en-v1.5"
 # agent.update_prompts({"agent_worker:system_prompt": react_system_prompt_small})
 
 # use query engine instead for now.
-Settings.chunk_size = 64
-Settings.chunk_overlap = 0
-documents = SimpleDirectoryReader(
-    input_files=["./README_small.md"]
-).load_data()
-index = VectorStoreIndex.from_documents(documents)
+# Settings.chunk_size = 64
+# Settings.chunk_overlap = 0
+# documents = SimpleDirectoryReader(
+#     input_files=["./README_small.md"]
+# ).load_data()
+# index = VectorStoreIndex.from_documents(documents)
 
-query_engine = index.as_query_engine(
-    verbose=True,
-    similarity_top_k=1,
-    response_mode="compact",
-    streaming=True,
-)
+# query_engine = index.as_query_engine(
+#     verbose=True,
+#     similarity_top_k=1,
+#     response_mode="compact",
+#     streaming=True,
+# )
 
+
+import nest_asyncio
+nest_asyncio.apply()
 
 class MyBot(ActivityHandler):
 
     async def on_message_activity(self, turn_context: TurnContext):
-        # Send message to agent and get response
-        query = turn_context.activity.text
-        print(f"\nQuery: {query}")
-        start_time = time.time()
-        streaming_response = query_engine.query(query)
-        print("Answer: ", end="")
-        response = ""
-        for text in streaming_response.response_gen:
-            if text:
-                response += text
+        global repo_engine
 
-                # Send streaming
-                act = Activity(
-                    type="streaming",
-                    text=text,
-                )
-                await turn_context.send_activity(act)
+        message = turn_context.activity.text
+        owner, repo = extract_github_owner_repo(message)
 
-                # Print the streaming response to the console
-                print(text, end="", flush=True)
+        tps = 0
+        if owner and repo:
+            response = f"Thanks for sharing the link. Indexing {owner}/{repo} repo now."
+            act = Activity(
+                type="message",
+                text=response,
+                channel_data={"tokens_per_second": tps},
+            )
+            await turn_context.send_activity(act)
+            create_repo_engine(owner, repo)
+        else:
+            # Send message to agent and get response
+            query = turn_context.activity.text
+            print(f"\nQuery: {query}")
+            start_time = time.time()
+            streaming_response = repo_engine.query(query)
+            print("Answer: ", end="")
+            response = ""
+            for text in streaming_response.response_gen:
+                if text:
+                    response += text
 
-        elapsed_time = time.time() - start_time
-        tps = len(response.split()) / elapsed_time
+                    # Send streaming
+                    act = Activity(
+                        type="streaming",
+                        text=text,
+                    )
+                    await turn_context.send_activity(act)
 
-        # strip end characters
-        response = response.rstrip("</s>")
+                    # Print the streaming response to the console
+                    print(text, end="", flush=True)
 
-        # Send the entire response as a single message
-        act = Activity(
-            type="message",
-            text=response,
-            channel_data={"tokens_per_second": tps},
-        )
-        await turn_context.send_activity(act)
+            elapsed_time = time.time() - start_time
+            tps = len(response.split()) / elapsed_time
+
+            # strip end characters
+            response = response.rstrip("</s>")
+
+            # Send the entire response as a single message
+            act = Activity(
+                type="message",
+                text=response,
+                channel_data={"tokens_per_second": tps},
+            )
+            await turn_context.send_activity(act)
 
     async def on_members_added_activity(self, members_added: ChannelAccount, turn_context: TurnContext):
-        initial_greeting = "Hi I'm Neo, I can generate jokes for you. Just ask me to tell you a joke about anything. For example, 'tell me a joke about lemons'"
+        initial_greeting = "Hi I'm Neo, I can index github projects for you so you can easily query them. Just paste a link and I'll get on it! For example, 'please index this repo: https://github.com/onnx/turnkeyml'"
         for member_added in members_added:
             if member_added.id != turn_context.activity.recipient.id:
                 await turn_context.send_activity(initial_greeting)
