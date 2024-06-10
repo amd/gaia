d403402ce7e8bee975faca624cd4260a325d1e3f
danielholanda <holand.daniel@gmail.com>
2024-06-10 16:44:41 -0700

    opening and closing socket separately

--- src/gaia/agents/Example/app.py
+++ src/gaia/agents/Example/app.py
@@ -22,35 +22,36 @@ def launch_llm_server():
 
 class MyAgent:
     def __init__(self):
-        self.pipe = None
+        self.llm_server_websocket = None
+
+    def __del__(self):
+        if self.llm_server_websocket is not None:
+            self.llm_server_websocket.close()
 
     async def prompt_llm_server(self, prompt, ui_request):
-        uri = "ws://localhost:8000/ws"
 
-        async with websockets.connect(uri) as llm_server_websocket:
-            await llm_server_websocket.send(prompt)
+        await self.llm_server_websocket.send(prompt)
 
-            timeout_duration = 2
+        timeout_duration = 2
 
-            response = web.StreamResponse()
-            await response.prepare(ui_request)
-            while True:
-                try:
-                    # Receive messages
-                    token = await asyncio.wait_for(
-                        llm_server_websocket.recv(), timeout=timeout_duration
-                    )
-                    if token:
-                        print(token)
-                        if token == "</s>":
-                            break
-                        await response.write(f"{token}\n".encode("utf-8"))
-                except asyncio.TimeoutError:
-                    # No token received for a while. Ending communication
-                    break
-            await response.write_eof()
-            await llm_server_websocket.close()
-            return response
+        response = web.StreamResponse()
+        await response.prepare(ui_request)
+        while True:
+            try:
+                # Receive messages
+                token = await asyncio.wait_for(
+                    self.llm_server_websocket.recv(), timeout=timeout_duration
+                )
+                if token:
+                    print(token)
+                    if token == "</s>":
+                        break
+                    await response.write(f"{token}\n".encode("utf-8"))
+            except asyncio.TimeoutError:
+                # No token received for a while. Ending communication
+                break
+        await response.write_eof()
+        return response
 
     async def on_message_received(self, ui_request):
         data = await ui_request.json()
@@ -70,6 +71,11 @@ class MyAgent:
         llm_thread = Thread(target=launch_llm_server)
         llm_thread.daemon = True
         llm_thread.start()
+
+        uri = "ws://localhost:8000/ws"
+
+        # async with websockets.connect(uri) as llm_server_websocket:
+        self.llm_server_websocket = await websockets.connect(uri)
         response = {"status": "Success"}
         return web.json_response(response)
 
